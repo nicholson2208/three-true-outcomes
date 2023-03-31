9cc36d7fb17d73bd18566743124b10da1b94e2d5
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2023-03-31 16:33:04 -0600

    change the run date to be yesterday

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def main():
     
     # figure out the date and time to run
     # Github actions will be utc
-    #run_dt = str(datetime.date.today() + datetime.timedelta(days=-1))
-    run_dt = '2022-06-16'
+    run_dt = str(datetime.date.today() + datetime.timedelta(days=-1))
+    #run_dt = '2022-06-16'
     
     tto_df = pd.DataFrame()
     tto_events_df = pd.DataFrame()
