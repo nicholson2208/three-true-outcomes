01dd55c979f98b88563b2b41758a8991b14fe0af
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2023-05-11 16:05:43 -0600

    added a slightly prettier thing

--- simple_line.py
+++ simple_line.py
@@ -1,16 +1,48 @@
 from bokeh.plotting import figure, show, output_file
+from bokeh.models import ColumnDataSource, DataTable, DateFormatter, TableColumn
+from bokeh.layouts import row
+import pandas as pd
 
 # prepare some data
 x = [1, 2, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
 
+
+df = pd.read_csv("data/tto_events.csv")
+
+plot_data = df["events"].value_counts()
+outcomes = list(plot_data.index)
+# print(plot_data.columns)
+
+source = ColumnDataSource(df)
+
+columns = [
+        TableColumn(field="batter", title="Batter"),
+        TableColumn(field="pitch_type", title="Pitch Type"),
+        TableColumn(field="release_speed", title="Pitch Speed"),
+        TableColumn(field="events", title="Outcome"),
+        TableColumn(field="launch_speed", title="Exit Velo"),
+    ]
+data_table = DataTable(source=source, 
+                       columns=columns, 
+                       width=400, 
+                       height=280)
+
 # create a new plot with a title and axis labels
-p = figure(title="Simple line example", x_axis_label='x', y_axis_label='y')
+p = figure(title="Count of Three True Outcome Events",
+            x_range=outcomes,
+            x_axis_label='outcomes',
+            y_axis_label='frequency',
+            tools="hover",
+            width=400, 
+            height=280)
 
 # add a line renderer with legend and line thickness to the plot
-p.line(x, y, legend_label="Temp.", line_width=2)
 
-output_file('index.html')
+p.vbar(x=plot_data.index, bottom=0, top=plot_data.values,
+        width=0.8)
 
 # show the results
-show(p)
+show(row(p, data_table))
+
+output_file('index.html')
